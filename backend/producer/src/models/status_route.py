7d1af3184ed351ba5ac4b9e3328e98b12693fe92
xh4vm <h4vm@yandex.ru>
2023-11-04 17:24:40 +0500

    feat: Валидаторы входных данных

--- backend/producer/src/models/status_route.py
+++ backend/producer/src/models/status_route.py
@@ -18,6 +18,20 @@ class Geo(JSONModel):
     latitude: float
     longitude: float
 
+    @validator('latitude')
+    def latitude_ge_zero(cls, value: float) -> float:
+        if value >= 0:
+            return value
+
+        raise ValueError('"latitude" must be greater than zero')
+    
+    @validator('longitude')
+    def longitude_ge_zero(cls, value: float) -> float:
+        if value >= 0:
+            return value
+
+        raise ValueError('"longitude" must be greater than zero')
+
 
 class RouteData(BaseModel):
     train_id: uuid.UUID
@@ -28,11 +42,16 @@ class RouteData(BaseModel):
     geo: Geo
 
     @validator('speed')
-    def frame_time_ge_zero(cls, value: float) -> float:
+    def speed_ge_zero(cls, value: float) -> float:
+        if value >= 0:
+            return value
+        
+    @validator('num_railway_carriage')
+    def num_ge_zero(cls, value: float) -> float:
         if value >= 0:
             return value
 
-        raise ValueError('"speed" must be greater than zero')
+        raise ValueError('"num_railway_carriage" must be greater than zero')
 
 class StatusRoute(RouteData):
     datetime: str = Field(default_factory=lambda: dt.now().strftime('%Y-%m-%d %H:%M:%S'))
